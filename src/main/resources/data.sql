f2d202b51987cd365f0e55c69cafc69c316700dc
Angela <Angela_shalievska@outlook.com>
2021-04-08 21:54:51 +0200

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+insert into roles
+values (1, 'admin'),
+       (2, 'tutor'),
+       (3, 'student');
+
+insert into subjects
+values (1, 'Mathematics'),
+       (2, 'History'),
+       (3, 'English'),
+       (4, 'Art'),
+       (5, 'Music'),
+       (6, 'Science'),
+       (7, 'Structural Programming'),
+       (8, 'Advanced Programming'),
+       (9, 'Object Oriented Programming'),
+       (10, 'Calculus I'),
+       (11, 'Calculus II'),
+       (12, 'Probability & Statistics'),
+       (13, 'Marketing'),
+       (14, 'Cognitive Science'),
+       (15, 'Artificial Intelligence');
